4a9c48db5ffa087a5e0e741773db5a0b238cb76b
wsb <woosb0115@nate.com>
2022-09-29 22:08:23 +0900

    2022.09.29 ConnectionMaker 구현체를 가져오는 부분을 메소드 추출을 이용하여 리팩토링

--- src/main/java/toby/spring/user1_3/DaoFactory.java
+++ src/main/java/toby/spring/user1_3/DaoFactory.java
@@ -2,6 +2,22 @@ package toby.spring.user1_3;
 
 public class DaoFactory {
     public UserDao getUserDao(){
-        return new UserDao(new NConnectionMaker());
+        return new UserDao(getConnectionMaker());
+    }
+
+    /*
+    ## new NConnectionMaker()를 메소드 추출을 통해 리팩토링 시
+    ## 아래와 같이 중복 될 수 있는 부분을 제거해 준다.
+
+    public AccountDao getAccountDao(){
+        return new AccountDao(getConnectionMaker());
+    }
+    public MessageDao getMessageDao(){
+        return new MessageDao(getConnectionMaker());
+    }
+    */
+
+    private ConnectionMaker getConnectionMaker(){
+        return new NConnectionMaker();
     }
 }
