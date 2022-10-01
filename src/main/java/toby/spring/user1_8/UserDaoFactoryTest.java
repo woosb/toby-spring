9ad8b89f5fabbf9da3a1ae45fde8f462e93abd23
wsb <woosb0115@nate.com>
2022-10-01 17:18:35 +0900

    2022.10.01 xml 방식으로 DataSource 사용할 수 있도록 수정

--- src/main/java/toby/spring/user1_8/UserDaoFactoryTest.java
+++ src/main/java/toby/spring/user1_8/UserDaoFactoryTest.java
@@ -1,6 +1,7 @@
 package toby.spring.user1_8;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public class UserDaoFactoryTest {
     public static void main(String[] args) throws SQLException {
 
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext applicationContext = new GenericXmlApplicationContext("/WEB-INF/user1_8Context.xml");
         UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
 
         userDao.deleteAll();
